fc37c83b1c0b2a04d7e3dc1f6549499d768c311a
Darren Seet <darren.seet@kliqxe.com>
2024-05-29 00:40:54 +0800

    Remove irrelevant code and hardcoded values

--- force-app/main/default/lwc/nfcWriter/nfcWriter.js
+++ force-app/main/default/lwc/nfcWriter/nfcWriter.js
@@ -3,21 +3,21 @@ import { createRecord, getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { getNfcService, getBiometricsService } from 'lightning/mobileCapabilities';
 import { reduceErrors } from 'c/utils'
 import { ShowToastEvent} from 'lightning/platformShowToastEvent'
-import createLink from '@salesforce/apex/DocumentController.createLink'
 import createVCF from '@salesforce/apex/NFCController.createVCF'
 
 export default class NfcReader extends LightningElement {
     @api recordId;
     
-    text;
-
     nfcService
     biometricsService
-    connectedCallback() {
+    showToastEvent
+    @api invoke() {
         this.nfcService = getNfcService();
         this.biometricsService = getBiometricsService();
+        this.showToastEvent = new ShowToastEvent();
+        this.handleWriteClick();
     }
-    
+
     async verifyUser() {
         if (this.biometricsService.isAvailable()) {
             const options = {
@@ -27,11 +27,52 @@ export default class NfcReader extends LightningElement {
             const result = await this.biometricsService.checkUserIsDeviceOwner(options);
             return result;
         } else {
-            this.text = 'Problem initiating Biometrics service'
+            this.showToastEvent = {
+                title: 'Problem initiating biometricsService',
+                variant: 'error'
+            }
+            this.dispatchEvent(this.showToastEvent)        
         }
     }
-    
-    async handleReadClick() {
+
+    async handleWriteClick() {
+        let isUser = await this.verifyUser();
+        if (isUser && this.nfcService.isAvailable()) {
+            let url = ''
+            try {
+                url = await createVCF({ contactId: this.recordId })
+            } catch(error) {
+                this.showToastEvent = {
+                    title: 'Error creating Virtual Contact File',
+                    message: reduceErrors(error),
+                    variant: 'error',
+                }
+                this.dispatchEvent(this.showToastEvent)
+            }
+            const linkRecord = await this.nfcService.createUriRecord(url)
+            const payload = [linkRecord] 
+            const options = {
+              "instructionText": "Hold your phone near the tag to write.",
+              "successText": "Tag written successfully!"
+            };
+            this.nfcService.write(payload, options).then(() => {
+                this.showToastEvent = {
+                    title: 'Tag written successfully!',
+                    variant: 'success',
+                }
+                this.dispatchEvent(this.showToastEvent)
+            }).catch((error) => {
+                this.showToastEvent = {
+                    title: 'Error code: ' + error.code,
+                    message: error.message,
+                    variant: 'error'
+                }
+                this.dispatchEvent(this.showToastEvent)
+            });
+        }
+    }   
+
+    /* async handleReadClick() {
         let isUser = await this.verifyUser();
         if (isUser && this.nfcService.isAvailable()) {
             const options = {
@@ -47,36 +88,6 @@ export default class NfcReader extends LightningElement {
         } else {
             this.text = 'Problem initiating NFC service.'
         }
-    }
-
-    async handleWriteClick() {
-        let isUser = await this.verifyUser();
-        if (isUser && this.nfcService.isAvailable()) {
-            let url = ''
-            try {
-                url = await createVCF({ contactId: this.recordId })
-            } catch(error) {
-                this.dispatchEvent(new ShowToastEvent({
-                    title: 'Error creating VCF',
-                    message: reduceErrors(error),
-                    variant: 'error'
-                }))
-            }
-            const linkRecord = await this.nfcService.createUriRecord(url)
-            const payload = [linkRecord] 
-            const options = {
-              "instructionText": "Hold your phone near the tag to write.",
-              "successText": "Tag written successfully!"
-            };
-            this.nfcService.write(payload, options)
-              .then(() => {
-                this.text = "Tag written successfully!";
-              })
-              .catch((error) => {
-                // Handle errors
-                this.text = 'Error code: ' + error.code + '\nError message: ' + error.message;
-              });
-        }
-    }
+    } */
 }
 
